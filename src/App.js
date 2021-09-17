579b78c0dc4ad23c4101c30072bf60ad1845b8ce
Ryan Deschamps <ryan.deschamps@gmail.com>
2021-09-17 15:37:00 -0400

    First working application for React Template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import ShowMetaForm from './components/show-meta-form';
 import './App.css';
+import configData from './config.json';
+import { GoogleLogout, GoogleLogin } from 'react-google-login';
+
+
+
 
 function App() {
+  const clientId = configData.FIREBASE.clientId;
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
+  const [name, setName] = useState("guest");
+
+  const responseGoogle = response => {
+    setIsLoggedIn(true);
+    setName(response.profileObj.name);
+  }
+  const responseFail = response => {
+    console.log(response);
+    setIsLoggedIn(false);
+  }
+
+  const logout = response => {
+    setIsLoggedIn(false);
+    setName("guest");
+  }
+
+  useEffect(() => {
+
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        {isLoggedIn ? (
+                              <div>
+                                    Signed in as: <a href="#login">{name}</a>
+                                    
+                                <GoogleLogout
+                                clientId={clientId}
+                                buttonText="Logout"
+                                onLogoutSuccess={logout}
+                                  isSignedIn={isLoggedIn}>
+                                </GoogleLogout></div>
+                            ) : (
+                                <div><GoogleLogin
+                                clientId={clientId}
+                                buttonText="Login with Google"
+                                onSuccess={responseGoogle}
+                                onFailure={responseFail}
+                                cookiePolicy={'single_host_origin'}></GoogleLogin></div>
+                            )}
+                    
+           <ShowMetaForm user={name} />
+        </div>
+    
   );
 }
 
